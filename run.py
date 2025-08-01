6306a2be7dd9b54c274a68d0c860206c4c663e98
yossy538 <135330450+yossy538@users.noreply.github.com>
2025-08-02 06:40:09 +0900

    fix: 本番環境で自動マイグレーション実行を追加

--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 # run.py
 from dotenv import load_dotenv
-load_dotenv()  # ←本当にここで最初に呼ぶ！
+load_dotenv()
 
 import os
 from kouteihyo_app import create_app
 
 app = create_app()
+
+# ★ここで自動マイグレーション
+if os.environ.get("IS_PRODUCTION") == "true":
+    from flask_migrate import upgrade
+    with app.app_context():
+        upgrade()
 
 if __name__ == "__main__":
     print("[DEBUG] os.getcwd() =", os.getcwd())
